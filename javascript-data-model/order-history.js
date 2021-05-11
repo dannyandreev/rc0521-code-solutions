8288d3e022547aab11a2ad147bd6015b5a92a34b
dannyandreev <danny.andreev@gmail.com>
2021-05-11 10:44:14 -0700

    javascript-data-model | Danny Andreev

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,73 @@
+var orderHistory = [
+  {
+    number: '114-3941689-8772232',
+    datePlaced: 'August 4, 2020',
+    totalPrice: 34,
+    shipTo: 'JS Masher',
+    deliveryDate: 'Aug 8, 2020',
+    item: [
+      {
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr Axel',
+        portrait: 'image',
+        returnDate: 'Sep 7, 2020',
+        price: 31.55
+      }
+    ]
+  },
+  {
+    number: '113-9984268-1280257',
+    datePlaced: 'July 19, 2020',
+    totalPrice: 44.53,
+    shipTo: 'JS Masher',
+    deliveryDate: 'Jul 20, 2020',
+    item: [
+      {
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        portrait: 'image',
+        returnDate: 'Aug 19, 2020',
+        price: 41.33
+      }
+    ]
+  },
+  {
+    number: '114-2875557-9059409',
+    datePlaced: 'July 4, 2020',
+    totalPrice: 17.22,
+    shipTo: 'JS Masher',
+    deliveryDate: 'Jul 7, 2020',
+    item: [
+      {
+        title: 'Gamecube Controller adapter, Super Smash Bros Switch Gamecube Adapt rfor WII U, PC. Support Turbo and Vibration Features . No Driver and No Lag-Gamecube Adapter',
+        author: '',
+        portrait: 'image',
+        returnDate: 'Aug 5, 2020',
+        price: 15.98
+      }
+    ]
+  },
+  {
+    number: '113-2883177-2648248',
+    datePlaced: 'July 3, 2020',
+    totalPrice: 138.93,
+    shipTo: 'JS Masher',
+    deliveryDate: 'Jul 5, 2020',
+    item: [
+      {
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        author: '',
+        portrait: 'image',
+        returnDate: 'Aug 4, 2020',
+        price: 94.95
+      },
+      {
+        title: 'Art of Sql',
+        author: 'Faroult, Stephane',
+        portrait: 'image',
+        returnDate: 'Aug 4, 2020',
+        price: 33.99
+      }
+    ]
+  }
+];
