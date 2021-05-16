2d0c3d253cfa662df8fd37be025041939d39b813
dannyandreev <danny.andreev@gmail.com>
2021-05-15 18:53:09 -0700

    array-manipulation-2 | Danny Andreev

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,36 +1,19 @@
 /* exported chunk */
 function chunk(array, count) {
-  // var newArray = []
+  var newArray = [];
+  var groups = [];
 
-  // if(array.length===0) {
-  //   return newArray
-  // }
+  for (var element of array) {
+    groups.push(element);
+    if (groups.length === count) {
+      newArray.push(groups);
+      groups = [];
+    }
+  }
 
-  // console.log(array.length / count)
+  if (groups.length > 0) {
+    newArray.push(groups);
+  }
 
-  // for(var i = 0; i<(array.length / count);i++){
-  //   newArray.push([])
-  //   for(var j = 0; j<count;j++){
-  //     if(array.length===(j+1)*count+1) {
-  //       console.log('j+k '+(j+1)*count+1)
-  //     } else {
-  //       newArray[i].push([])
-  //     }
-  //   }
-  // }
-
-  // var j = 0
-  // var k = 0
-
-  // for(var i = 0; i<array.length; i++){
-  //   console.log(j+' '+k+' '+array[i])
-  //   newArray[j][k] = array[i]
-  //   k++;
-  //   if(k===count){
-  //     k = 0
-  //     j++;
-  //   }
-  // }
-  // console.log(newArray)
-  // return newArray
+  return newArray;
 }
