7baf1f86c56d8f9f56f6e9ed0d471ab790279c1d
dannyandreev <danny.andreev@gmail.com>
2021-06-02 10:57:21 -0700

    bank works

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,42 +1,37 @@
 /* exported Bank */
 function Bank() {
- this.nextAccountNumber = 1;
- this.accounts = [];
+  this.nextAccountNumber = 1;
+  this.accounts = [];
 }
 
-Bank.prototype.openAccount = function(holder, balance) {
-  if(!Number.isInteger(balance) || balance <= 0) {
+Bank.prototype.openAccount = function (holder, balance) {
+  if (!Number.isInteger(balance) || balance <= 0) {
     return null;
-  } else if(typeof holder !== 'string') {
+  } else if (typeof holder !== 'string') {
     return null;
   } else {
-    var newAccount = new Account(this.nextAccountNumber, holder)
-    newAccount.deposit(balance)
+    var newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
 
-  this.accounts.push(newAccount)
+    this.accounts.push(newAccount);
   }
   this.nextAccountNumber++;
-  return this.nextAccountNumber-1;
-}
+  return this.nextAccountNumber - 1;
+};
 
-
-Bank.prototype.getAccount = function(number) {
-  for(var i = 0; i < this.accounts.length; i++) {
-    console.log('accounts[i]: '+accounts[i])
-    console.log('accounts[i].number' + accounts[i].number)
-    if(accounts[i].number === number) {
-      console.log('account returned')
-      return account
+Bank.prototype.getAccount = function (number) {
+  for (var i = 0; i < this.accounts.length; i++) {
+    if (this.accounts[i].number === number) {
+      return this.accounts[i];
     }
   }
   return null;
-}
+};
 
-
-Bank.prototype.getTotalAssets = function() {
+Bank.prototype.getTotalAssets = function () {
   var getTotal = 0;
-  for (account in this.accounts){
-    getTotal += account.balance
+  for (var i = 0; i < this.accounts.length; i++) {
+    getTotal += this.accounts[i].getBalance();
   }
-  return getTotal
-}
+  return getTotal;
+};
