3d0bc54a0bf1edff9e18bda827f1e67a1140f944
dannyandreev <danny.andreev@gmail.com>
2021-07-13 11:21:31 -0700

    react-validated-input-component

--- react-validated-input-component/src/password.jsx
+++ react-validated-input-component/src/password.jsx
@@ -5,26 +5,25 @@ class Password extends React.Component {
   constructor(props) {
     super(props);
     this.state = { password: '', valid: false, error: 'A password is required' };
-    this.handleInput = this.handleInput.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
 
-  handleInput(event) {
-    this.setState({ password: event.target.value });
-    const passLength = this.state.password.length;
-    // console.log(`handle: ${passLength}`)
+  handleChange(event) {
+    this.setState({ password: event.target.value }, () => {
+      const passLength = this.state.password.length;
 
-    if (passLength === 0) {
-      this.setState({ valid: false, error: 'A password is required' });
-    } else if (passLength < 7) {
-      this.setState({ valid: false, error: 'Your password is too short' });
-    } else {
-      this.setState({ valid: true, error: '' });
-    }
-    this.render();
+      if (passLength === 0) {
+        this.setState({ valid: false, error: 'A password is required' });
+      } else if (passLength < 8) {
+        this.setState({ valid: false, error: 'Your password is too short' });
+      } else {
+        this.setState({ valid: true, error: '' });
+      }
+    });
   }
 
   render() {
-    // console.log(`render: ${this.state.password.length}`)
+
     let url = '../media/cross.png';
     if (this.state.valid) {
       url = '../media/check.png';
@@ -37,7 +36,7 @@ class Password extends React.Component {
         </div>
         <div className="flex">
           <form className="center">
-            <input type="password" value={this.state.value} onChange={this.handleInput}/>
+            <input type="password" value={this.state.value} onChange={this.handleChange}/>
           </form>
           <div className="center">
             <img className="icon" src={url}/>
