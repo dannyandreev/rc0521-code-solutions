5ba21b494891b70d3ffa8d38ecb3bc44f164c788
dannyandreev <danny.andreev@gmail.com>
2021-07-12 21:58:13 -0700

    still need to fix async issues

--- react-validated-input-component/src/index.jsx
+++ react-validated-input-component/src/index.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import Password from './password'
+import Password from './password';
 
 const element = (
   <div>
