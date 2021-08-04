7f2694aad8245ec710146a8da77e3d6cbb231c50
dannyandreev <danny.andreev@gmail.com>
2021-08-04 13:04:59 -0700

    complete

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,25 +1,20 @@
 /* exported takeNextSmallest */
 
-function takeNextSmallest(queue) {
 
+function takeNextSmallest(queue) {
   if(isEmpty(queue)){
     return undefined
   }
 
-  console.log(queue.print())
   let val = queue.dequeue()
   if(isEmpty(queue)){
     return val
   }
 
-
-  while(queue.peek()<=val){
-    console.log(queue.print())
-    val = queue.peek()
-    queue.enqueue(queue.dequeue())
+  while(val > queue.peek()){
+    queue.enqueue(val)
+    val = queue.dequeue()
   }
-  console.log(queue.print())
-  console.log(' ')
-  return val
 
+return val
 }
